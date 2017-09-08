36c66b152d016fc9e9d9fa5aa458131af21f31d5
Janobob <jan.werren@post.ch>
2017-09-09 01:12:02 +0200

    Projektdateien hinzufügen.

---
+++ Veloly-Backend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Veloly_Backend.MvcApplication" Language="C#" %>
---
+++ Veloly-Backend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Veloly_Backend.Startup))]
+namespace Veloly_Backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
