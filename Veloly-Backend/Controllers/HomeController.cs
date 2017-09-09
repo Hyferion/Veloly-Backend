1eb20413b328013d471bcbd9cee8aeba47c09005
Janobob <jan.werren@post.ch>
2017-09-09 05:14:48 +0200

    optimized data and api

--- Veloly-Backend/Controllers/HomeController.cs
+++ Veloly-Backend/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 using Veloly_Backend.Handler;
 using Veloly_Backend.JsonModels;
 
@@ -18,18 +19,34 @@ namespace Veloly_Backend.Controllers
 
         public async Task<ActionResult> Login()
         {
-            var handler = new APIHandler();
-            handler.Login();
+            var handler = new APIHandler
+            {
+                Action = "company/login/",
+                Values = new JavaScriptSerializer().Serialize(new
+                {
+                    username = "jan@werren.com",
+                    password = "Voyager88",
+                    companyDomain = "veloly"
+                })
+            };
             var json = new Json {JsonString = await handler.RequestPostAsync()};
-            return View(json);
+            return View("Json", json);
         }
 
         public async Task<ActionResult> LockGetAll()
         {
-            var handler = new APIHandler();
-            handler.LockGetAll();
-            var json = new Json {JsonString = await handler.RequestPostAsync()};
-            return View(json);
+            var handler = new APIHandler
+            {
+                Action = "lock/get/all/",
+                Values = new JavaScriptSerializer().Serialize(new
+                {
+                    username = "jan@werren.com",
+                    password = "Voyager88",
+                    companyDomain = "veloly"
+                })
+            };
+            var json = new Json { JsonString = await handler.RequestPostAsync() };
+            return View("Json",json);
         }
     }
 }
